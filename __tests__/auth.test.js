ff4b13889836a90628e3157b888da86ea4dad07b
luthfan-ap <luthfan@gmail.com>
2025-06-03 10:14:27 +0700

    Fixed auth.test.js wordings etc

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -37,28 +37,28 @@ describe('Authentication Functions', () => {
       const password = 'weakpassword';
       const errorMessage = 'Firebase: The email address is already in use by another account (auth/email-already-in-use).';
       const error = new Error(errorMessage);
-
-      // Make the mock function reject with an error
+      
+      // reject pakai error
       mockCreateUserWithEmailAndPassword.mockImplementationOnce(() => Promise.reject(error));
-
+      
       await expect(register(email, password)).rejects.toThrow(error);
-
+      
       expect(global.firebase.auth).toHaveBeenCalledTimes(1);
       expect(mockCreateUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
       expect(mockCreateUserWithEmailAndPassword).toHaveBeenCalledWith(email, password);
     });
   });
-
+  
   describe('login', () => {
     it('should call firebase.auth().signInWithEmailAndPassword with correct credentials on success', async () => {
       const email = 'existing@example.com';
       const password = 'securepassword';
-
+      
       await login(email, password);
-
-      // Expect firebase.auth() to have been called
+      
+      // expect firebase.auth() dipanggil
       expect(global.firebase.auth).toHaveBeenCalledTimes(1);
-      // Expect signInWithEmailAndPassword to have been called with the provided email and password
+      // expect signInWithEmailAndPassword dipanggil pakai email dan password mock
       expect(mockSignInWithEmailAndPassword).toHaveBeenCalledTimes(1);
       expect(mockSignInWithEmailAndPassword).toHaveBeenCalledWith(email, password);
     });
@@ -69,7 +69,7 @@ describe('Authentication Functions', () => {
       const errorMessage = 'Firebase: There is no user record corresponding to this identifier. The user may have been deleted (auth/user-not-found).';
       const error = new Error(errorMessage);
 
-      // Make the mock function reject with an error
+      // reject pakai error
       mockSignInWithEmailAndPassword.mockImplementationOnce(() => Promise.reject(error));
 
       await expect(login(email, password)).rejects.toThrow(error);
