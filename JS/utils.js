94491136268d7e98d2eaf512c3a34566742d8019
luthfan-ap <luthfan@gmail.com>
2025-06-09 21:21:36 +0700

    Revert "minor fix 2"
    This reverts commit 2db55355f3a94ef24ed716a596f86eaab112e929.

--- JS/utils.js
+++ JS/utils.js
@@ -63,6 +63,7 @@ function changeTheme(color) {
         profilePicture.classList.remove('standard-profile-picture', 'light-profile-picture', 'darker-profile-picture');
         profilePicture.classList.add(`${color}-profile-picture`);
     }
+    // --- AKHIR PERBAIKAN UNTUK HALAMAN PROFIL ---
 }
 
 // Membuat fungsi-fungsi ini tersedia secara global di objek `window` untuk Browser.
